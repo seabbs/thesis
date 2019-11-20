33c130fd89faf14b5669c0752be76b5620e20d91
Sam Abbott <sam.abbott@bristol.ac.uk>
2019-11-20 11:28:28 +0000

    Allowed for git pull and gitignored data archive

--- bin/grab_projects.sh
+++ bin/grab_projects.sh
@@ -3,28 +3,28 @@
 # REQUIRED: git
 
 #Move into required directory
-printf "\tMaking project storage directory: %s \n" $1
-# Remove if exists
-rm -r -f $1
+printf "\tMaking (moving to) project storage directory: %s \n" $1
 # Make new directory
 mkdir $1
 # Move into new directory
 cd $1
 
 # Declare an array of projects
-declare -a projects=("DirectEffBCGPolicyChange" "ETSMissing" 
+declare -a projects=("DirectEffBCGPolicyChange" "ETSMissing"
                      "AssessBCGPolicyChange" "ModelTBBCGEngland"
-                     "tbinenglanddataclean" "ExploreBCGOnOutcomes") 
+                     "tbinenglanddataclean" "ExploreBCGOnOutcomes")
 
 
 # Iterate the projects array using for loop
 # git clone each project
-for project in ${projects[@]}; 
-do
-   printf "\tCloning project: %s into projects: %s\n" $project $1
-   git clone "https://github.com/seabbs/$project.git"
+for project in ${projects[@]}; do
+  if ([ -e $project ]); then
+    printf "\tUpdating project: %s \n" $project
+    cd $project
+    git pull
+    cd ..
+  else
+    printf "\tCloning project: %s into projects: %s\n" $project $1
+    git clone "https://github.com/seabbs/$project.git"
+  fi
 done
-
-
-# Move out of specified directory
-cd ..
